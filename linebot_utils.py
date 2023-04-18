b31ae6082573424af1bc0ddeb56ac7f32f80cce0
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-18 17:19:40 +0700

    Update linebot_utils.py

--- linebot_utils.py
+++ linebot_utils.py
@@ -1,9 +1,9 @@
 from chatgpt_utils import generate_chatgpt_response
 from dalle_utils import generate_dalle_image
-from linebot.models import ImageSendMessage
+from linebot.models import TextSendMessage, ImageSendMessage
 
 def handle_text_message(text):
-    if text.lower().startswith("รูป:"):
+    if text.lower().startswith("create image:"):
         image_prompt = text[13:].strip()
         image_url = generate_dalle_image(image_prompt)[0]
         return ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
