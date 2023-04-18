3b0e2a97c759b841f7d21dcb569993de82d8f538
thailanddynamic <nextgendevoffice@gmail.com>
2023-04-18 19:40:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-from linebot_utils import handle_text_message
+from chatgpt_utils import generate_chatgpt_response
 
 app = Flask(__name__)
 
@@ -17,13 +17,14 @@ logger = logging.getLogger(__name__)
 line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
-async def reply_message(reply_token, text):
-    await asyncio.sleep(2) # add a sleep to simulate a delay
-    message = TextSendMessage(text=text)
-    line_bot_api.reply_message(reply_token, message)
+async def reply_message(reply_token, response):
+    try:
+        await line_bot_api.reply_message(reply_token, TextSendMessage(text=response))
+    except Exception as e:
+        logger.error(e)
 
 @app.route('/webhook', methods=['POST'])
-def webhook():
+async def webhook():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     logger.info(f"Request body: {body}")
@@ -37,15 +38,9 @@ def webhook():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    reply_token = event.reply_token
-    prompt = event.message.text
-
-    # Generate response asynchronously
-    response = asyncio.run(handle_text_message(prompt))
-
-    # Reply to the user's message
-    asyncio.create_task(reply_message(reply_token, response))
+async def handle_message(event):
+    reply_message_task = asyncio.create_task(reply_message(event.reply_token, generate_chatgpt_response(event.message.text)))
+    await reply_message_task
 
 if __name__ == "__main__":
-    app.run()
+    asyncio.run(app.run(debug=True))
