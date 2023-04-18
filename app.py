8b1c3fb7338bb3084b45f366d21734729a7cc3e8
thailanddynamic <nextgendevoffice@gmail.com>
2023-04-18 18:49:16 +0700

    Revert

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-# Add this import line at the top
-from dalle_utils import generate_dalle_image
-
-# Rest of the app.py content
 
 from linebot_utils import handle_text_message
 
@@ -38,11 +34,6 @@ def webhook():
 def handle_message(event):
     reply_message = handle_text_message(event.message.text)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    reply_message = handle_text_message(event.message.text)
-    line_bot_api.reply_message(event.reply_token, reply_message)
 
 if __name__ == "__main__":
     app.run()
--- linebot_utils.py
+++ linebot_utils.py
@@ -1,12 +1,5 @@
 from chatgpt_utils import generate_chatgpt_response
-from dalle_utils import generate_dalle_image
-from linebot.models import ImageSendMessage
-from linebot.models import TextSendMessage
 
 def handle_text_message(text):
-    if text.lower().startswith("img:"):
-        image_prompt = text[13:].strip()
-        image_url = generate_dalle_image(image_prompt)[0]
-        return ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
-    else:
-        return TextSendMessage(text=generate_chatgpt_response(text))
+    response_text = generate_chatgpt_response(text)
+    return response_text
