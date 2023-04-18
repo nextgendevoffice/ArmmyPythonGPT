c77dd948bf40b4728ad3b9b74b9636fdbb1c504b
thailanddynamic <nextgendevoffice@gmail.com>
2023-04-18 18:30:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,10 @@ def handle_message(event):
     reply_message = handle_text_message(event.message.text)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    reply_message = handle_text_message(event.message.text)
+    line_bot_api.reply_message(event.reply_token, reply_message)
+
 if __name__ == "__main__":
     app.run()
