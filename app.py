5f33536fdd59639737e0a727c44fa7d14d30b39b
thailanddynamic <nextgendevoffice@gmail.com>
2023-04-18 18:28:11 +0700

    fix

--- app.py
+++ app.py
@@ -39,10 +39,5 @@ def handle_message(event):
     reply_message = handle_text_message(event.message.text)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    reply_message = handle_text_message(event.message.text)
-    line_bot_api.reply_message(event.reply_token, reply_message)
-
 if __name__ == "__main__":
     app.run()
--- linebot_utils.py
+++ linebot_utils.py
@@ -3,7 +3,7 @@ from dalle_utils import generate_dalle_image
 from linebot.models import ImageSendMessage
 
 def handle_text_message(text):
-    if text.lower().startswith("รูป:"):
+    if text.lower().startswith("img:"):
         image_prompt = text[13:].strip()
         image_url = generate_dalle_image(image_prompt)[0]
         return ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
