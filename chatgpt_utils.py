3c3ba654cb28cd4dd07d831e0bd835691b4f365a
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-18 17:04:49 +0700

    Update Logs

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -1,5 +1,8 @@
 import requests
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 def generate_chatgpt_response(prompt):
     api_key = os.environ['OPENAI_API_KEY']
@@ -20,5 +23,9 @@ def generate_chatgpt_response(prompt):
     response = requests.post('https://api.openai.com/v1/chat/completions', json=data, headers=headers)
     response_data = response.json()
     generated_text = response_data['choices'][0]['message']['content'].strip()
+
+    # Log the prompt and generated response
+    logger.info(f"User prompt: {prompt}")
+    logger.info(f"Generated response: {generated_text}")
 
     return generated_text
