24c1502b6417fae279e98863db957c097bc12733
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-19 17:16:21 +0700

    Update_IMAGES

--- app/line_bot.py
+++ app/line_bot.py
@@ -1,9 +1,10 @@
 import os
 import logging
+import openai
 from flask import Blueprint, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 from .chatgpt import generate_response
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
@@ -15,6 +16,19 @@ LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def generate_dalle_image(prompt):
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="512x512",
+        response_format="url"
+    )
+
+    image_url = response['data'][0]['url']
+    return image_url
 
 @line_bp.route("/callback", methods=['POST'])
 def callback():
@@ -33,6 +47,13 @@ def handle_message(event):
     logging.info("Handling event: %s", event)
 
     text = event.message.text
-    response = generate_response(text)
-    logging.info("Generated response: %s", response)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+
+    if text.startswith('/img'):
+        prompt = text[4:].strip()
+        image_url = generate_dalle_image(prompt)
+        image_message = ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        response = generate_response(text)
+        logging.info("Generated response: %s", response)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
