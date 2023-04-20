75eb0908c99674e5b414b0e0f63e0ce1dffe09bb
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-20 16:23:48 +0700

    Update thai_translation_model.py

--- app/thai_translation_model.py
+++ app/thai_translation_model.py
@@ -1,15 +1,9 @@
-from transformers import MarianMTModel, MarianTokenizer
-
-MODEL_NAME = "Helsinki-NLP/opus-mt-th-en"
-
-tokenizer = MarianTokenizer.from_pretrained(MODEL_NAME)
-model = MarianMTModel.from_pretrained(MODEL_NAME)
+from googletrans import Translator
 
 def preprocess(text):
     return text
 
 def translate(text):
-    input_ids = tokenizer.encode(text, return_tensors="pt")
-    output_ids = model.generate(input_ids)[0]
-    english_text = tokenizer.decode(output_ids, skip_special_tokens=True)
-    return english_text
+    translator = Translator()
+    translated_text = translator.translate(text, src='th', dest='en').text
+    return translated_text
