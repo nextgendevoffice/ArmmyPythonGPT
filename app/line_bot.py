284b3d5dd18b2057e0173fc9ca23baf25686c774
nextgendevoffice <nextgendevoffice@gmail.com>
2023-04-20 17:24:24 +0700

    Update line_bot.py

--- app/line_bot.py
+++ app/line_bot.py
@@ -12,7 +12,6 @@ from . import thai_translation_model
 from . import image_generation_model
 import random
 import string
-from .database import Coupons, CouponUsage
 from .database import Coupons, CouponUsage, User
 
 
